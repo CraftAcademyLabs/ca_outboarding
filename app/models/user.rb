77ada68bad31dd249228a27d0f56f72ec94c62fc
Thomas <thomas@craftacademy.se>
2018-10-20 17:52:15 +0200

    refactors user#full_name adds enum for role

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,11 +4,13 @@ class User < ApplicationRecord
   validates_length_of :first_name, minimum: 3, maximum: 50, allow_blank: true
   validates_length_of :last_name, minimum: 3, maximum: 50, allow_blank: true
 
+  enum role: { member: 0 }
+
   def full_name
     [first_name, last_name].join(' ') if first_name && last_name
-    end
+  end
 
   def display_name
-    full_name.squish.empty? ? email : full_name
+    self.full_name.blank? ? self.email : self.full_name
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -21,31 +21,35 @@ RSpec.describe User, type: :model do
     end
   end
 
-  describe "Instance methods" do
-    let(:user_without_name) { create(:member, email: "no_name@random.com", first_name: nil, last_name: nil) }
-    let(:user_with_name) { create(:member, first_name: "John", last_name: "Doe") }
+  describe 'Instance methods' do
+    let(:user_without_name) { create(:member, email: 'no_name@random.com', first_name: nil, last_name: nil) }
+    let(:user_with_name) { create(:member, first_name: 'John', last_name: 'Doe') }
 
     it { is_expected.to respond_to :full_name }
     it { is_expected.to respond_to :display_name }
 
-    describe "#full_name" do
-      it "user with name attributes set returns full name" do
-        expect(user_with_name.full_name).to eq "John Doe"
+    describe '#full_name' do
+      it 'user with name attributes set returns full name' do
+        expect(user_with_name.full_name).to eq 'John Doe'
       end
 
-      it "user without name attributes set returns false" do
+      it 'user without name attributes set returns false' do
         expect(user_without_name.full_name).to be_falsy
       end
     end
 
-    describe "#display_name" do
-      it "user with name attributes set returns full name" do
-        expect(user_with_name.display_name).to eq "John Doe"
+    describe '#display_name' do
+      it 'user with name attributes set returns full name' do
+        expect(user_with_name.display_name).to eq 'John Doe'
       end
 
-      it "user without name attributes set returns false" do
-        expect(user_without_name.display_name).to eq "no_name@random.com"
+      it 'user without name attributes set returns false' do
+        expect(user_without_name.display_name).to eq 'no_name@random.com'
       end
     end
+  end
+
+  describe 'Class methods' do
+    it { expect(described_class).to respond_to :member }
   end
 end
