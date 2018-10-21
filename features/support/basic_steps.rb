7726fc95933f6dd2bbe357262162739cc02acfae
Thomas <thomas@craftacademy.se>
2018-10-21 12:52:52 +0200

    adds and configures devise gem

--- features/support/basic_steps.rb
+++ features/support/basic_steps.rb
@@ -1,9 +1,14 @@
 # frozen_string_literal: true
 
-When('a visitor visits the site') do
+When('a visitor/member visits the site') do
   visit root_path
 end
 
 Then('stop') do 
   binding.pry
+end
+
+Given("member {string} is logged in") do |email|
+  user = User.find_by_email(email)
+  sign_in(user)
 end
