328e1f226da5459966d5bb8f1b04d63e5508776b
Thomas <thomas@craftacademy.se>
2018-10-23 07:39:55 +0200

    adds custom registrations and sessions controller customizes login and sign up views

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+class SessionsController < Devise::SessionsController
+  respond_to :js
+
+  def new
+    super
+  end
+
+  def create
+    self.resource = warden.authenticate(auth_options)
+    if resource
+      sign_in(resource_name, resource)
+      flash[:notice] = 'Signed in successfully.'
+      redirect_to root_path
+    else
+      render json: { message: 'Could not log you in' }, status: :unauthorized
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: {
+    registrations: :registrations,
+    sessions: :sessions
+  }
   root controller: :members, action: :index
   resources :members, only: [:show, :edit, :update]
 end
