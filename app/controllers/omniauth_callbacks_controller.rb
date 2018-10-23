ec444c633dddbb97f020436fb9ff73bbf97fcfed
Magnus <magnus.thor.ar@gmail.com>
2018-10-23 16:56:22 +0200

    add callback for CaX

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -13,4 +13,26 @@ class OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def failure
     redirect_to root_path, notice: 'Could not authenticate you!'
   end
+
+  def cognitive_class
+    callback_from :cognitive_class
+  end
+  
+  private
+
+  def callback_from(provider)
+    provider = provider.to_s
+    @user = User.find_for_oauth(request.env['omniauth.auth'])
+    if @user.persisted?
+      flash[:success] = I18n.t('devise.omniauth_callbacks.success', kind: provider.capitalize)
+      sign_in_and_redirect @user, event: :authentication
+    else
+      if provider == 'twitter'
+        session["devise.#{provider}_data"] = request.env['omniauth.auth'].except('extra')
+      else
+        session["devise.#{provider}_data"] = request.env['omniauth.auth']
+      end
+      redirect_to new_user_registration_url
+    end
+  end
 end
