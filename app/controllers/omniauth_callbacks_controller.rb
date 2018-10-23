69ac6387e14dab2f4c97fe4b5473fdde3ff7aaf5
oliverochman <oliver.ochman@hotmail.com>
2018-10-23 10:20:46 +0200

    [WIP] add from_omniauth to user model and define linkedin method in oauth callbacks controller

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -1,5 +1,12 @@
 class OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def linkedin
-
+    @user = User.from_omniauth(request.env["omniauth.auth"])
+    if @user.persisted? 
+        sign_in_and_redirect @user, event: :authentication
+        set_flash_message(:notice, :success, kind: 'LinkedIn') if is_navigational_format?
+    else
+        session['devise.linkedin_date'] = request.env['omniauth.auth']
+        redirect_to new_user_registration_path #or should it be url?
+    end
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -18,4 +18,11 @@ class User < ApplicationRecord
   def display_name
     self.full_name.blank? ? self.email : self.full_name
   end
+
+  def self.from_omniauth(auth)
+    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+      user.email = auth.info.email
+      user.password = Devise.friendly_token[0,20]
+    end
+  end
 end
