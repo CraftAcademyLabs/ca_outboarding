5419f189db9b5832f64f1d374d1e0767d9627976
oliverochman <oliver.ochman@hotmail.com>
2018-10-23 10:13:03 +0200

    setup routes for omniauth callbacks

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: { omniauth_callbacks: :omniauth_callbacks }
   root controller: :members, action: :index
   resources :members, only: [:show, :edit, :update]
 end
