c20879ab0a7d513c0e307811a7e44cb9a3607877
faraznaeem <faraznaeem@gmail.com>
2018-11-21 14:09:23 +0100

    Rename MembersIndex to UsersIndex

--- app/indexes/members_index.rb
+++
@@ -1,5 +0,0 @@
-class MembersIndex < Chewy::Index
-    define_type Member.all do
-        field :first_name
-    end
-end 
---
+++ app/indexes/users_index.rb
@@ -0,0 +1,5 @@
+class UsersIndex < Chewy::Index
+define_type User.all do
+        field :first_name
+    end
+end 
