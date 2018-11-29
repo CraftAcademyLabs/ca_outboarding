2ad72eae32dcdaee38d3e859a996c025ac1306c2
Magnus <magnus.thor.ar@gmail.com>
2018-11-29 16:09:00 +0100

    styled the about us page

--- app/javascript/css/tailwind.js
+++ app/javascript/css/tailwind.js
@@ -64,6 +64,8 @@ let colors = {
   'grey-light': '#dae1e7',
   'grey-lighter': '#f1f5f8',
   'grey-lightest': '#f8fafc',
+  'grey-light-shade': '#ddd',
+  'ca-grey': '#afafaf',
   'white': '#ffffff',
 
   'red-darkest': '#3b0d0c',
