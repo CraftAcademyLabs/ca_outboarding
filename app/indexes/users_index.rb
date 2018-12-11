4f57d6a8e7b4d5e9cdbfe3541577aad209d50cca
Noel Ryan <noelgerard.ryan@gmail.com>
2018-12-11 09:31:11 +0100

    Fixed last merge conflict

--- app/indexes/users_index.rb
+++ app/indexes/users_index.rb
@@ -1,13 +1,5 @@
 class UsersIndex < Chewy::Index
     define_type User.all do
-<<<<<<< HEAD
-        field :first_name
-        field :last_name
-        field :age
-        field :role
-        field :gender
-=======
         field :first_name, :last_name, :gender
->>>>>>> f6861bfe009e250fc2f1ebba904682d754c84244
     end
 end
